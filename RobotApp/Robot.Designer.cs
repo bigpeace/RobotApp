44e5e9e82cd9dc12d0bfe8fdbe5a32ffb2661215
Yamato-Suzuki <44799293+bigpeace@users.noreply.github.com>
2022-08-24 08:00:02 +0900

    first commit

---
+++ RobotApp/Robot.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace RobotApp
+{
+    partial class Robot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Robot));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lbl_top = new System.Windows.Forms.Label();
+            this.pb_top = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pb_robot = new System.Windows.Forms.PictureBox();
+            this.lbl_copyright = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_destination = new System.Windows.Forms.Label();
+            this.lb_operation = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_top)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_robot)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.HSplit;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.splitContainer1.Panel1.Controls.Add(this.lbl_top);
+            this.splitContainer1.Panel1.Controls.Add(this.pb_top);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel2);
+            this.splitContainer1.Panel2.Controls.Add(this.lbl_copyright);
+            this.splitContainer1.Panel2.Controls.Add(this.statusStrip1);
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Size = new System.Drawing.Size(1590, 712);
+            this.splitContainer1.SplitterDistance = 51;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // lbl_top
+            // 
+            this.lbl_top.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_top.AutoSize = true;
+            this.lbl_top.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_top.Location = new System.Drawing.Point(63, 9);
+            this.lbl_top.Name = "lbl_top";
+            this.lbl_top.Size = new System.Drawing.Size(215, 34);
+            this.lbl_top.TabIndex = 1;
+            this.lbl_top.Text = "Robot Simulator";
+            // 
+            // pb_top
+            // 
+            this.pb_top.Image = global::RobotApp.Properties.Resources.agv_25;
+            this.pb_top.Location = new System.Drawing.Point(13, 4);
+            this.pb_top.Name = "pb_top";
+            this.pb_top.Size = new System.Drawing.Size(44, 44);
+            this.pb_top.TabIndex = 0;
+            this.pb_top.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.Controls.Add(this.pb_robot);
+            this.panel2.Location = new System.Drawing.Point(316, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1262, 619);
+            this.panel2.TabIndex = 3;
+            // 
+            // pb_robot
+            // 
+            this.pb_robot.Location = new System.Drawing.Point(172, 69);
+            this.pb_robot.Name = "pb_robot";
+            this.pb_robot.Size = new System.Drawing.Size(906, 530);
+            this.pb_robot.TabIndex = 0;
+            this.pb_robot.TabStop = false;
+            // 
+            // lbl_copyright
+            // 
+            this.lbl_copyright.AutoSize = true;
+            this.lbl_copyright.Location = new System.Drawing.Point(730, 630);
+            this.lbl_copyright.Name = "lbl_copyright";
+            this.lbl_copyright.Size = new System.Drawing.Size(269, 23);
+            this.lbl_copyright.TabIndex = 2;
+            this.lbl_copyright.Text = "Copyright@2022 Persol-R&D Co.,Ltd.";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 631);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1590, 26);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(63, 20);
+            this.toolStripStatusLabel1.Text = "ver 0.0.0";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.lbl_destination);
+            this.panel1.Controls.Add(this.lb_operation);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.btn_stop);
+            this.panel1.Controls.Add(this.btn_start);
+            this.panel1.Location = new System.Drawing.Point(12, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(298, 619);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbl_destination
+            // 
+            this.lbl_destination.AutoSize = true;
+            this.lbl_destination.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_destination.Location = new System.Drawing.Point(23, 62);
+            this.lbl_destination.Name = "lbl_destination";
+            this.lbl_destination.Size = new System.Drawing.Size(73, 30);
+            this.lbl_destination.TabIndex = 4;
+            this.lbl_destination.Text = "行先：";
+            // 
+            // lb_operation
+            // 
+            this.lb_operation.FormattingEnabled = true;
+            this.lb_operation.ItemHeight = 23;
+            this.lb_operation.Location = new System.Drawing.Point(23, 254);
+            this.lb_operation.Name = "lb_operation";
+            this.lb_operation.Size = new System.Drawing.Size(250, 349);
+            this.lb_operation.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("メイリオ", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(23, 95);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(248, 33);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(159, 176);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(112, 34);
+            this.btn_stop.TabIndex = 1;
+            this.btn_stop.Text = "button2";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(23, 176);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(112, 34);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "button1";
+            this.btn_start.UseVisualStyleBackColor = true;
+            // 
+            // Robot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1590, 712);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Robot";
+            this.Text = "Robot";
+            this.Load += new System.EventHandler(this.Robot_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_top)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_robot)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label lbl_top;
+        private System.Windows.Forms.PictureBox pb_top;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbl_copyright;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl_destination;
+        private System.Windows.Forms.ListBox lb_operation;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.PictureBox pb_robot;
+    }
+}
---
+++ RobotApp/Robot.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace RobotApp
+{
+    public partial class Robot : Form
+    {
+        public Robot()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Robot_Load(object sender, EventArgs e)
+        {
+            // 描画先とするImageオブジェクトを作成する
+            Bitmap canvas = new Bitmap(pb_robot.Width, pb_robot.Height);
+            // Imageオブジェクト の Graphicsオブジェクトを作成する
+            Graphics g = Graphics.FromImage(canvas);
+
+            // 直線で接続する点の配列を作成
+            Point[] ps = {
+                new Point(240, 60),
+                new Point(660, 60),
+                new Point(660, 480),
+                new Point(240, 480),
+                new Point(240, 60)
+            };
+
+            // ダッシュ
+            Pen LinePen = new Pen(Color.LightSkyBlue, 5);
+            LinePen.DashStyle = DashStyle.Dash;
+
+            g.DrawLines(LinePen, ps);
+
+            // 画像ファイルを読み込んで、Imageオブジェクトとして取得する
+            Image robotImg = Properties.Resources.agv;
+            // 画像をcanvasの座標(240 ,60)の位置に描画する → ここでDBから呼び出すようにする
+            g.DrawImage(robotImg, 240 - 30, 60 - 30, robotImg.Width / 3, robotImg.Height / 3);
+
+            // リソースを解放する
+            g.Dispose();
+            // pb_robotに表示する
+            pb_robot.Image = canvas;
+
+        }
+    }
+}
